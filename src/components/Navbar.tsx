0255ef3ef19f5b4a96a9140e46ce7c7fa2b747f1
Guilhermecheng <guilherme.cheng@gmail.com>
2023-01-24 21:32:40 -0300

    mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -108,53 +108,82 @@ export function Navbar() {
     ]
 
     return (
-        <div id="sidebar" className='flex flex-col items-center bg-zinc-100 dark:bg-zinc-900 px-4 justify-center h-64 w-full laptop:fixed laptop:w-[40%] laptop:h-screen'>
-            <div id="mobile-menu" className="flex absolute right-4 top-4 laptop:hidden text-zinc-800 dark:text-zinc-100">
-                { isMobileMenuOpen ? <IoMdClose size={40} onClick={() => setIsMobileMenuOpen(false)} /> : <HiMenuAlt1 size={40} onClick={() => setIsMobileMenuOpen(true)} /> }
-            </div>
+        <>
+            <div className={`${ isMobileMenuOpen ? "block" : "hidden" } absolute z-20 bg-zinc-100 dark:bg-zinc-900 w-screen h-[100%] flex flex-col items-center text-center`}>
+                    <div className="nlock h-40"></div>
+                    <ul className="mt-50">
+                        { pages.map((page: PageProps) => {
+                            return (
+                                <li key={page.id} className="text-zinc-500 dark:text-zinc-100 text-xl uppercase mb-2">
+                                    <Link  className={`hover:text-amber-400 ${currentRoute === page.href && "text-amber-400"}`} href={page.href} passHref>
+                                        { language === "ptBR" ? page.textPTBR : page.textEN }
+                                    </Link>
+                                </li>
+                            )
+                        }) }
+                    </ul>
+                    <Switch.Root className="SwitchRoot mb-2" id="dark-mode" checked={darkModeChecked} onCheckedChange={ () => setDarkModeAtPage(darkModeChecked) }>
+                        <Switch.Thumb className="SwitchThumb" />
+                    </Switch.Root>
 
-            <div className="flex flex-row items-center justify-center mt-6 laptop:mt-0">
-                <div className='w-32'>
-                    <img src="https://github.com/Guilhermecheng.png"  alt="Guilherme's GitHub profile picture" className='rounded-full object-center border-4 border-amber-400' />
+                    <div id="language" className=" text-zinc-500 dark:text-zinc-100">
+                        <span>Language: <span>{ language === "ptBR" ? (
+                            <span className="cursor-pointer text-zinc-800 dark:text-amber-400 hover:underline hover:underline-offset-1" onClick={() => setLanguage("EN")}>BR</span>
+                        ) : (
+                            <span className="cursor-pointer text-zinc-800 dark:text-amber-400 hover:underline hover:underline-offset-1" onClick={() => setLanguage("ptBR")}>EN</span>
+                        ) }</span></span>
+                    </div>
+                </div>
+            <div id="sidebar" className='flex flex-col items-center bg-zinc-100 dark:bg-zinc-900 px-4 justify-center h-64 w-full laptop:fixed laptop:w-[40%] laptop:h-screen'>
+                
+
+                <div id="mobile-menu" className="flex absolute right-4 top-4 z-40 laptop:hidden text-zinc-800 dark:text-amber-400">
+                    { isMobileMenuOpen ? <IoMdClose size={40} onClick={() => setIsMobileMenuOpen(false)} /> : <HiMenuAlt1 size={40} onClick={() => setIsMobileMenuOpen(true)} /> }
                 </div>
 
-                <div className='ml-4'>
-                    <h1 className="text-zinc-800 dark:text-zinc-100 text-xl font-bold laptop:text-3xl">Guilherme Cheng</h1>
-                    <h3 className="text-zinc-500 dark:text-zinc-300 text-sm laptop:text-lg">front end dev, mechanical engineer </h3>
+                <div className="flex flex-row items-center justify-center mt-6 laptop:mt-0">
+                    <div className='w-32'>
+                        <img src="https://github.com/Guilhermecheng.png"  alt="Guilherme's GitHub profile picture" className='rounded-full object-center border-4 border-amber-400' />
+                    </div>
+
+                    <div className='ml-4'>
+                        <h1 className="text-zinc-800 dark:text-zinc-100 text-xl font-bold laptop:text-3xl">Guilherme Cheng</h1>
+                        <h3 className="text-zinc-500 dark:text-zinc-300 text-sm laptop:text-lg">front end dev, mechanical engineer </h3>
+                    </div>
+                </div>
+
+                <div id="inspirational-phrase" className="mt-6 pb-6 laptop:pb-0 laptop:mt-12 text-zinc-500 dark:text-zinc-300 relative">
+                    <p className="italic">
+                        {/* Lorem, the famous ipsum! */}
+                        { phrase && phrase.phrase }
+                    </p>
+                    <p className="absolute right-0 font-semibold">
+                        { phrase && phrase.author }
+                    </p>
+                </div>
+
+                <div id="navbar-menu" className="hidden mt-16 text-zinc-500 dark:text-zinc-100 uppercase laptop:flex laptop:flex-col laptop:items-center laptop:space-y-1">
+                    { pages.map((page: PageProps) => {
+                        return (
+                            <Link key={page.id} className={`hover:text-amber-400 ${currentRoute === page.href && "text-amber-400"}`} href={page.href} passHref>
+                                { language === "ptBR" ? page.textPTBR : page.textEN }
+                            </Link>
+                        )
+                    }) }
+                </div>
+
+                <Switch.Root className="SwitchRoot hidden laptop:block fixed bottom-10 left-10" id="dark-mode" checked={darkModeChecked} onCheckedChange={ () => setDarkModeAtPage(darkModeChecked) }>
+                    <Switch.Thumb className="SwitchThumb" />
+                </Switch.Root>
+
+                <div id="language" className="absolute bottom-10 hidden laptop:block text-zinc-500 dark:text-zinc-100">
+                    <span>Language: <span>{ language === "ptBR" ? (
+                        <span className="cursor-pointer text-zinc-800 dark:text-amber-400 hover:underline hover:underline-offset-1" onClick={() => setLanguage("EN")}>BR</span>
+                    ) : (
+                        <span className="cursor-pointer text-zinc-800 dark:text-amber-400 hover:underline hover:underline-offset-1" onClick={() => setLanguage("ptBR")}>EN</span>
+                    ) }</span></span>
                 </div>
             </div>
-
-            <div id="inspirational-phrase" className="mt-6 pb-6 laptop:pb-0 laptop:mt-12 text-zinc-500 dark:text-zinc-300 relative">
-                <p className="italic">
-                    {/* Lorem, the famous ipsum! */}
-                    { phrase && phrase.phrase }
-                </p>
-                <p className="absolute right-0 font-semibold">
-                    { phrase && phrase.author }
-                </p>
-            </div>
-
-            <div id="navbar-menu" className="hidden mt-16 text-zinc-500 dark:text-zinc-100 uppercase laptop:flex laptop:flex-col laptop:items-center laptop:space-y-1">
-                { pages.map((page: PageProps) => {
-                    return (
-                        <Link key={page.id} className={`hover:text-amber-400 ${currentRoute === page.href && "text-amber-400"}`} href={page.href} passHref>
-                            { language === "ptBR" ? page.textPTBR : page.textEN }
-                        </Link>
-                    )
-                }) }
-            </div>
-
-            <Switch.Root className="SwitchRoot hidden laptop:block" id="dark-mode" checked={darkModeChecked} onCheckedChange={ () => setDarkModeAtPage(darkModeChecked) }>
-                <Switch.Thumb className="SwitchThumb" />
-            </Switch.Root>
-
-            <div id="language" className="absolute bottom-8 hidden laptop:block text-zinc-500 dark:text-zinc-100">
-                <span>Language: <span>{ language === "ptBR" ? (
-                    <span className="cursor-pointer hover:underline hover:underline-offset-1" onClick={() => setLanguage("EN")}>BR</span>
-                ) : (
-                    <span className="cursor-pointer hover:underline hover:underline-offset-1" onClick={() => setLanguage("ptBR")}>EN</span>
-                ) }</span></span>
-            </div>
-        </div>
+        </>
     )
 }
