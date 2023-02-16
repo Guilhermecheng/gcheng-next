a9520d353804f6776371b9daaa28fceff4d3072d
Guilhermecheng <guilherme.cheng@gmail.com>
2023-02-16 00:15:54 -0300

    bug fix

--- src/components/ProjectThumb.tsx
+++ src/components/ProjectThumb.tsx
@@ -13,15 +13,14 @@ interface ProjectProps {
 }
 
 interface ProjectThumbProps {
-    key: number;
     project: ProjectProps;
     onClickFunction: (proj: ProjectProps) => void;
     language: string;
 }
 
-export function ProjectThumb({ key, onClickFunction, project, language }: ProjectThumbProps) {
+export function ProjectThumb({ onClickFunction, project, language }: ProjectThumbProps) {
     return (
-        <li key={key} onClick={ () => onClickFunction(project) } className="relative flex flex-col w-full rounded-lg overflow-hidden dark:border-2 dark:border-zinc-500 hover:border-2 hover:border-amber-400 hover:dark:border-amber-400 hover:cursor-pointer group">
+        <li onClick={ () => onClickFunction(project) } className="relative flex flex-col w-full rounded-lg overflow-hidden dark:border-2 dark:border-zinc-500 hover:border-2 hover:border-amber-400 hover:dark:border-amber-400 hover:cursor-pointer group">
 
             <img src={project.image} alt={project.imgAlt} className='w-full bg-center bg-no-repeat' />
             <div className="absolute bottom-0 w-full h-full flex items-center justify-center bg-zinc-700 opacity-0 laptop:opacity-0 laptop:group-hover:opacity-80 transition ease-in-out duration-300 z-20 cursor-pointer">
--- src/pages/contact/Form.tsx
+++ src/pages/contact/Form.tsx
@@ -34,10 +34,11 @@ export function Form() {
         <div className="mt-4 laptop:mt-6 w-full rounded-md">
         
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4 mb-8 laptop:mb-0">
-                <input {...register("name")} placeholder={placeholderLang.name} className="rounded-md bg-zinc-300 dark:bg-zinc-900 py-2 px-4 text-zinc-800 dark:text-zinc-100 focus:outline-none focus:border-2 focus:border-amber-400" />
-                <input {...register("email")} placeholder={placeholderLang.email} className="rounded-md bg-zinc-300 dark:bg-zinc-900 py-2 px-4 text-zinc-800 dark:text-zinc-100 focus:outline-none focus:border-2 focus:border-amber-400" />
-                <textarea {...register("message")} placeholder={placeholderLang.message} className=" rounded-md bg-zinc-300 dark:bg-zinc-900 py-2 px-4 h-40 laptop:h-60 text-zinc-800 dark:text-zinc-100 focus:outline-none focus:border-2 focus:border-amber-400" />
-                <input type="submit" className="px-4 py-2 bg-amber-400 laptop:w-40  text-zinc-800 cursor-pointer rounded-md hover:bg-amber-300 hover:text-zinc-700" />
+                <input {...register("name")} placeholder={placeholderLang.name} className="rounded-md bg-zinc-200 dark:bg-zinc-900 py-2 px-4 text-zinc-800 dark:text-zinc-100 focus:outline-none focus:border-2 focus:border-amber-400" />
+                <input {...register("email")} placeholder={placeholderLang.email} className="rounded-md bg-zinc-200 dark:bg-zinc-900 py-2 px-4 text-zinc-800 dark:text-zinc-100 focus:outline-none focus:border-2 focus:border-amber-400" />
+                <textarea {...register("message")} placeholder={placeholderLang.message} className=" rounded-md bg-zinc-200 dark:bg-zinc-900 py-2 px-4 h-40 laptop:h-60 text-zinc-800 dark:text-zinc-100 focus:outline-none focus:border-2 focus:border-amber-400" />
+
+                <input type="submit" className="px-4 py-2 bg-amber-400 laptop:w-40  text-zinc-800 cursor-pointer rounded-md hover:bg-amber-300 hover:text-zinc-700 focus:outline-none focus:border-2 focus:border-blue-600" />
             </form>
         </div>
     )
