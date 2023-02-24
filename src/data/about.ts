972c3f76caea3288b9a2d0aa5901372549f57d70
Guilhermecheng <guilherme.cheng@gmail.com>
2023-02-24 02:26:07 -0300

    about page 1

---
+++ src/data/about.ts
@@ -0,0 +1,150 @@
+export const aboutPageContent = {
+    ptBR: {
+        title: "Guilherme Cheng",
+        subtitle: "Sobre mim, minhas habilidades e outras curiosidades!",
+        about: {
+            title: "Sobre",
+            text: [
+                "Desde criança sempre fui apaixonado por tecnologia. No entanto, minha jornada neste mundo começou recentemente, em 2020. Ao começar meus estudos em programação, me interessei pelo mundo do front end. Comecei estudando o HTML, CSS e o Javascript. Após me aprofundar muito neste mundo, descobri o React e suas tecnologias.",
+                "Hoje em dia trabalho com ReactJS, NextJS, Typescript e outras tecnologias para entregar o melhor e mais atualizado stack para o desenvolvimento frontend.",
+                "Além disso, sou um cara apaixonado por música, carros e esportes em geral. Costumo correr, jogar bola e tocar guitarra em meu tempo livre!"
+            ],
+        },
+        
+        curiosities: {
+            title:"",
+            text: [],
+        },
+        experience: {
+            title: "Experiência",
+            text:"",
+            expHistory: [
+                {
+                    title: "Frontend developer",
+                    company: "Guilherme Cheng",
+                    timeFrom: "jul/20",
+                    timeTo: "Today",
+                    tasks: [
+                        "Manter a qualidade e padrão de serviço da equipe de processamento de reclamações de garantia no suporte contratado do Acordo de Nível de Serviço.",
+                    ],
+                },
+                {
+                    title: "Business Analyst",
+                    company: "Accenture",
+                    timeFrom: "jul/22",
+                    timeTo: "Atualmente",
+                    tasks: [
+                        "Manter a qualidade e padrão de serviço da equipe de processamento de reclamações de garantia no suporte contratado do Acordo de Nível de Serviço.",
+                        "Investigar e verificar reclamações de garantia com base nos recursos de suporte externo disponíveis (catálogo de peças, assistência ao revendedor e tempo de trabalho padrão) e tomar a decisão apropriada.",
+                        "Implementar práticas para melhorar a eficiência operacional.",
+                        "Instruir e treinar membros da equipe.",
+                    ],
+                },
+                {
+                    title: "Analista de Garantia",
+                    company: "MSX International",
+                    timeFrom: "dez/21",
+                    timeTo: "jul/22",
+                    tasks: [],
+                },
+            ]
+        }
+    },
+    en: {
+        title: "Guilherme Cheng",
+        subtitle: "About me, my habilities and ohter curiosities!",
+        about: {
+            title: "About",
+            text: [
+                "Since I was a child, I've been a enthusiast of technology. But my journey started only recently, in 2020. Started my studies seeing a bit of everything, but fell in love with front end stack. So, I focused my studies, starting with HTML, CSS and JavaScript, and getting deep into ReactJS and its technologies. ",
+                "Today, I use ReactJS, NextJS, Typescript and all of its tools to deliver the best experience possible in frontend applications.",
+                "Also, I love music, cars and sports in general. I run, play football and guitar in my spare time!"
+            ]
+        },
+        
+        curiosities: {
+            title:"",
+            text: [],
+        },
+        experience: {
+            title: "Experience",
+            text:"",
+            expHistory: [
+                
+                {
+                    title: "Business Analyst",
+                    company: "Accenture",
+                    timeFrom: "jul/22",
+                    timeTo: "Today",
+                    tasks: [
+                        "Manter a qualidade e padrão de serviço da equipe de processamento de reclamações de garantia no suporte contratado do Acordo de Nível de Serviço.",
+                        "Investigar e verificar reclamações de garantia com base nos recursos de suporte externo disponíveis (catálogo de peças, assistência ao revendedor e tempo de trabalho padrão) e tomar a decisão apropriada.",
+                        "Implementar práticas para melhorar a eficiência operacional.",
+                        "Instruir e treinar membros da equipe.",
+                    ],
+                },
+                {
+                    title: "Warranty Analyst",
+                    company: "MSX International",
+                    timeFrom: "december 21",
+                    timeTo: "july 22",
+                    tasks: [
+
+                    ],
+                },
+            ]
+        }
+    },
+    habilities: {
+        titlePTBR: "Habilidades",
+        titleEN: "Habilities",
+        list: [
+            {
+                name: "ReactJS",
+                image: "/react.svg",
+                // image: <img src="/react.svg" alt="React" width={50} height={50} />,
+
+            },
+            {
+                name: "NextJS",
+                image: "/next-js.svg",
+            },
+            {
+                name: "Typescript",
+                image: "/typescript.svg",
+            },
+            {
+                name: "TailwindCSS",
+                image: "/tailwindcss.svg",
+            },
+            {
+                name: "Javascript",
+                image: "/javascript.svg",
+            },
+            {
+                name: "HTML 5",
+                image: "/html.svg",
+            },
+            {
+                name: "CSS",
+                image: "/css.svg",
+            },
+            {
+                name: "GraphQL",
+                image: "/graphql.svg",
+            },
+            {
+                name: "Python",
+                image: "/python.svg",
+            },
+            {
+                name: "NodeJS",
+                image: "/nodejs.svg",
+            },
+            {
+                name: "Firebase",
+                image: "/firebase.svg",
+            },
+        ]
+    }
+}
