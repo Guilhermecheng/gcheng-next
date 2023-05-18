9da8f7d4c19c8da7bcf24f510eea86a9f9717eff
Guilhermecheng <guilherme.cheng@gmail.com>
2023-05-18 00:34:51 -0300

    modal bug fix

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useRef } from 'react';
 import Link from "next/link";
 
 import * as Dialog from '@radix-ui/react-dialog';
@@ -7,22 +7,12 @@ import { BsBoxArrowUpRight, BsGithub } from 'react-icons/bs';
 
 import { GlobalContext } from '@/contexts/Contexts';
 import { StackBlock } from './StackBlock';
-import { useState } from 'react';
 
 
 export function Modal() {
     const { language, modalContent } = useContext(GlobalContext);
     const imgRef = useRef(null);
-    const [projDesc, setProjDesc] = useState(modalContent.description);
-
-    useEffect(() => {
-        if(language === "EN") {
-            setProjDesc(modalContent.descriptionEN)
-        }
-        else {
-            setProjDesc(modalContent.description)
-        }
-    }, [language])
+    let projDescription = language === "ptBR" ? modalContent.description : modalContent.descriptionEN;
 
     return (
         <Dialog.Portal>
@@ -81,7 +71,7 @@ export function Modal() {
                         </section>
 
                         <section className='px-6 pt-4 mb-2 max-h-[40vh] overflow-auto scrollbar-thin scrollbar-thumb-zinc-300 scrollbar-track-white dark:scrollbar-thumb-zinc-600 dark:scrollbar-track-zinc-800'>
-                            {projDesc.map((paragraph: string, i: number) => {
+                            {projDescription.map((paragraph: string, i: number) => {
                                 return (
                                     <p key={i} className="mb-2 last:mb-0 text-sm laptop:text-base first:text-xl first:laptop:text-lg first:font-semibold first:mb-4">{paragraph}</p>
                                 )
