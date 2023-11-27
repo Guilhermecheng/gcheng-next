a193150fd0695ed119fd6152fa5274e9a69c68aa
Guilhermecheng <guilherme.cheng@gmail.com>
2023-11-27 20:36:04 -0300

    blog post styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ export function Layout({ children }: LayoutProps) {
         <div className="flex flex-col tablet:items-center laptop:items-start w-full laptop:h-screen">
             <Navbar />
 
-            <div className="px-4 pt-4 laptop:px-12 laptop:w-[60%] laptop:h-screen laptop:absolute laptop:right-0">
+            <div className="w-full px-4 pt-4 laptop:px-12 laptop:w-[60%] laptop:h-screen laptop:absolute laptop:right-0">
                 {children}
             </div>
         </div>
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -5,7 +5,10 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 //@ts-ignore
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import { ParsedUrlQuery } from 'querystring'
+import { ParsedUrlQuery } from 'querystring';
+
+import { BsFillPersonFill, BsClockFill } from "react-icons/bs";
+
 
 interface IParams extends ParsedUrlQuery {
   slug: string
@@ -15,13 +18,25 @@ export default function Post({ postData, username, slug }: any) {
 
     return (
         <div className="text-zinc-700 dark:text-zinc-200">
-            <h1>{postData.title}</h1>
-            <div>{username}</div>
+          <div className="px-4 md:px-8">
+
+            <h1 className="pt-8 text-2xl laptop:text-4xl font-semibold text-zinc-600 dark:text-amber-400">{postData.title}</h1>
+            <div className="flex gap-x-8">
+              <div className="flex gap-x-2 items-center my-2 text-zinc-400">
+                <BsFillPersonFill size={16} />
+                <span>{username}</span>
+              </div>
+              <div className="flex gap-x-2 items-center text-zinc-400">
+                <BsClockFill size={16} />
+                <span>{postData.readable_publish_date}</span>
+              </div>
+            </div>
+          </div>
 
             <section className="block w-full px-4 md:px-8 py-10 text-base-text gap-y-2">
                 <ReactMarkdown
                     children={postData.body_markdown} 
-                    className="gap-y-2 prose prose-blue max-w-none" 
+                    className="gap-y-2 prose prose-blue max-w-none blog-content" 
                     components={{
                         code({node, inline, className, children, ...props}) {
                           const match = /language-(\w+)/.exec(className || '')
